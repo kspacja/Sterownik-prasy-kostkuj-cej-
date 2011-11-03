6f9520f9c725bc78d56adcde1f66982b09416172
unknown <Ja@.(none)>
2011-11-03 18:09:13 +0100

    Nowa wersja

--- GUI/CommandLn.java
+++ GUI/CommandLn.java
@@ -43,6 +43,12 @@ public static String fileName="";
 
     }
 
+    public static String createSensCommand(){
+        String command ="";
+        command = "cd "+path+"\n" + "java "+fileName+"\n";
+        return(command);
+    }
+
     public static void CommandLn(String silnik, int stopnie, int obroty, int czas, int moc){
     try
     {
@@ -73,6 +79,54 @@ public static String fileName="";
 
         processInput.write(commandToSend);
         processInput.flush();        
+
+        int lineCounter = 0;
+        while(true){
+            String line = processOutput.readLine();
+            if(line == null) break;
+            System.out.println(++lineCounter + ": " + line);
+        }
+
+        processInput.close();
+        processOutput.close();
+        pr.waitFor();
+    }
+        catch(Exception x)
+        {
+            x.printStackTrace();
+        }
+    }
+
+    public static void CommandSen(String silnik, int stopnie, int obroty, int czas, int moc){
+    try
+    {
+        Runtime rt = Runtime.getRuntime();
+
+        String command = "cmd";
+        String commandToSend ="";
+        Process pr = rt.exec(command);
+
+        BufferedReader processOutput = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+        BufferedWriter processInput = new BufferedWriter(new OutputStreamWriter(pr.getOutputStream()));
+
+
+
+        //Decyzja o trybie
+        if(stopnie!=0){
+            commandToSend = createCommand(silnik, stopnie, obroty, czas, moc);
+        }
+        else if(obroty!=0){
+            commandToSend = createCommand(silnik, stopnie, obroty, czas, moc);
+        }
+        else if(czas!=0){
+            commandToSend = createCommand(silnik, stopnie, obroty, czas, moc);
+        }
+        else
+        commandToSend = "notepad\n"+path+"\n"+"exit\n";
+        //commandToSend = "dir\n" + "exit\n";
+
+        processInput.write(commandToSend);
+        processInput.flush();
 
         int lineCounter = 0;
         while(true){
